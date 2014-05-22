7f05c589f34f5450d8f5e49ba303f23dd775653d
jenna hildebrand <jenna.hildebrand@gmail.com>
2014-05-21 21:21:42 -0700

    added css suckas

--- app/helpers/umadbro_helper.rb
+++ app/helpers/umadbro_helper.rb
@@ -3,7 +3,11 @@ module UmadbroHelper
   def random_mad
     umad = ["you mad bro?",
       "bro, you mad?",
-      "are you mad?"]
+      "are you mad?",
+      "bro why are you so mad",
+      "do you need some madicine because you're so mad?",
+      "bro you're so mad, are you from madison?",
+      "is your name madeline because of how mad you are?"]
 
     prompt = umad[rand(umad.count)]
   end
