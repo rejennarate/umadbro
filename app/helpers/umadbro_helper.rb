fe34a52326e853b9c521af8635f400065b57fffb
jenna hildebrand <jenna.hildebrand@gmail.com>
2014-05-21 21:35:06 -0700

    edited umadbro_helper.rb to remove a nonsensical mad

--- app/helpers/umadbro_helper.rb
+++ app/helpers/umadbro_helper.rb
@@ -4,10 +4,11 @@ module UmadbroHelper
     umad = ["you mad bro?",
       "bro, you mad?",
       "are you mad?",
-      "bro why are you so mad",
+      "bro are you mad because of your hurt butt",
       "do you need some madicine because you're so mad?",
       "bro you're so mad, are you from madison?",
-      "is your name madeline because of how mad you are?"]
+      "is your name madeline because of how mad you are?"
+      ]
 
     prompt = umad[rand(umad.count)]
   end
